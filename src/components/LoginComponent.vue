f988800c3463a9b0ce0e97ca7b5b25797a23a04a
Frontend <guirogeriobertagna00@gmail.com>
2025-03-24 17:08:01 -0300

    coisas feitas

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,42 +1,100 @@
 <template>
-  <div class="login-container">
+  <div class="container-page">
     <div class="logo-container">
       <img src="@/assets/logo.png" class="logo-img" alt="Logo">
     </div>
     
-    <form @submit.prevent="handleLogin" class="login-card">
-      <h1 class="login-title">Login</h1>
-      
-      <div class="form-group">
-        <label for="email">Email:</label>
-        <input
-          id="email"
-          type="email"
-          v-model="emailUser"
-          @blur="emailTouched = true"
-          placeholder="exemple@exemple"
-          class="form-input"
-          :class="{ 'input-error': emailTouched && emailError }"
-        >
-        <p v-if="emailTouched && emailError" class="error-message">{{ emailError }}</p>
+    <div class="login-container">
+      <form v-if="isLoginMode" @submit.prevent="handleLogin" class="login-card">
+        <h1 class="login-title">Login</h1>
+
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input
+            id="email"
+            type="email"
+            v-model="emailUser"
+            @blur="emailTouched = true"
+            placeholder="exemple@exemple"
+            class="form-input"
+            :class="{ 'input-error': emailTouched && emailError }"
+          >
+          <p v-if="emailTouched && emailError" class="error-message">{{ emailError }}</p>
+        </div>
+
+        <div class="form-group">
+          <label for="password">Password:</label>
+          <input
+            id="password"
+            type="password"
+            v-model="passwordUser"
+            @blur="passwordTouched = true"
+            class="form-input"
+            :class="{ 'input-error': passwordTouched && passwordError }"
+          >
+          <p v-if="passwordTouched && passwordError" class="error-message">{{ passwordError }}</p>
+        </div>
+
+        <button class="login-button" type="submit" :disabled="!isFormValid">Login</button>
+        <p v-if="loginError" class="error-message">{{ loginError }}</p>
+      </form>
+
+
+
+
+
+
+
+      <form v-else @submit.prevent="handleRegister" class="login-card">
+        <h1 class="login-title">Register</h1>
+
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input
+              @blur="nameTouched = true"
+            id="name"
+            v-model="nameUserRegister"
+            class="form-input"
+            :class="{ 'input-error': nameTouched && nameErrorRegister }"
+          >
+          <p v-if="nameTouched && nameErrorRegister" class="error-message">{{ nameErrorRegister }}</p>
+        </div>
+
+        <div class="form-group">
+          <label for="emailRegister">Email:</label>
+          <input
+            id="emailRegister"
+            type="email"
+            v-model="emailUserRegister"
+            @blur="emailTouchedRegister = true"
+            placeholder="exemple@exemple"
+            class="form-input"
+            :class="{ 'input-error': emailTouchedRegister && emailErrorRegister }"
+          >
+          <p v-if="emailTouchedRegister && emailErrorRegister" class="error-message">{{ emailErrorRegister }}</p>
+        </div>
+
+        <div class="form-group">
+          <label for="passwordRegister">Password:</label>
+          <input
+            id="passwordRegister"
+            type="password"
+            v-model="passwordUserRegister"
+            @blur="passwordTouchedRegister = true"
+            class="form-input"
+            :class="{ 'input-error': passwordTouchedRegister && passwordErrorRegister }"
+          >
+          <p v-if="passwordTouchedRegister && passwordErrorRegister" class="error-message">{{ passwordErrorRegister }}</p>
+        </div>
+
+        <button class="login-button" :disabled="!isFormValidRegister" type="submit">Register</button>
+        <p v-if="loginError" class="error-message">{{ loginError }}</p>
+      </form>
+      <div class="toggle-mode">
+        <p v-if="isLoginMode">Don't have an account? <a href="#" @click.prevent="toggleMode">Register</a></p>
+        <p v-else>Already have an account? <a href="#" @click.prevent="toggleMode">Login</a></p>
       </div>
-      
-      <div class="form-group">
-        <label for="password">Password:</label>
-        <input
-          id="password"
-          type="password"
-          v-model="passwordUser"
-          @blur="passwordTouched = true"
-          class="form-input"
-          :class="{ 'input-error': passwordTouched && passwordError }"
-        >
-        <p v-if="passwordTouched && passwordError" class="error-message">{{ passwordError }}</p>
-      </div>
-      
-      <button class="login-button" type="submit" :disabled="!isFormValid">Login</button>
-      <p v-if="loginError" class="error-message">{{ loginError }}</p>
-    </form>
+    </div>
   </div>
 </template>
 
@@ -44,16 +102,25 @@
 import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import { useAuthenticateStore } from '@/stores/authenticate';
-import { login } from '@/service/HttService';
+import { login, register } from '@/service/HttService';
 
 const router = useRouter();
 const loginStore = useAuthenticateStore();
 
 const emailUser = ref('');
 const passwordUser = ref('');
+const emailUserRegister = ref('');
+const passwordUserRegister = ref('');
+const nameUserRegister = ref('');
 const loginError = ref('');
 const emailTouched = ref(false);
+const nameTouched = ref(false)
+const emailTouchedRegister = ref(false)
+const passwordTouchedRegister = ref (false)
 const passwordTouched = ref(false);
+
+// Estado para alternar entre login e registro
+const isLoginMode = ref(true);
 
 const emailError = computed(() => {
   if (!emailUser.value) return 'O e-mail é obrigatório.';
@@ -66,9 +133,25 @@ const passwordError = computed(() => {
   if (passwordUser.value.length < 6) return 'A senha deve ter pelo menos 6 caracteres.';
   return '';
 });
+const emailErrorRegister = computed(() => {
+  if (!emailUserRegister.value) return 'O e-mail é obrigatório.';
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailUserRegister.value)) return 'E-mail inválido.';
+  return '';
+});
 
+const passwordErrorRegister = computed(() => {
+  if (!passwordUserRegister.value) return 'A senha é obrigatória.';
+  if (passwordUserRegister.value.length < 6) return 'A senha deve ter pelo menos 6 caracteres.';
+  return '';
+});
+
+const nameErrorRegister = computed(() => {
+  if (!nameUserRegister.value) return 'o nome é obrigatório.';
+  if (!/[A-Z]/.test(nameUserRegister.value)) return 'O nome deve conter pelo menos uma letra maiúscula.';
+  return '';
+});
 const isFormValid = computed(() => !emailError.value && !passwordError.value);
-
+const isFormValidRegister= computed(() => !emailErrorRegister.value && !passwordErrorRegister.value);
 async function handleLogin() {
   emailTouched.value = true;
   passwordTouched.value = true;
@@ -89,22 +172,63 @@ async function handleLogin() {
     loginError.value = 'Falha no login. Verifique suas credenciais.';
   }
 }
+
+async function handleRegister() {
+  emailTouchedRegister.value = true;
+  passwordTouchedRegister.value = true;
+
+  try {
+    const result = await register({name: nameUserRegister.value, email: emailUserRegister.value, password: passwordUserRegister.value });
+    
+    if (result?.data) {
+      loginStore.saveUser(result.data);
+      router.push('/');
+      alert('Registro sucesso');
+      console.log(result.data)
+    } else {
+      throw new Error('Credenciais inválidas.');
+    }
+  } catch (error) {
+    loginError.value = 'Falha no registro. Verifique suas credenciais.';
+  }
+}
+
+// Função para alternar entre login e registro
+function toggleMode() {
+  isLoginMode.value = !isLoginMode.value;
+}
 </script>
 
 <style scoped>
-.login-container {
+.container-page {
   display: flex;
-  flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   min-height: 100vh;
-  background-color: #f5f5f5;
-  font-family: 'Big Shoulders', sans-serif;
 }
 
 .logo-container {
-  max-width: 150px;
-  margin-bottom: 1rem;
+  background-color: var(--secondary-color-orange);
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  min-width: 100vh;
+}
+
+.logo-img {
+  width: 250px;
+  height: 250px;
+}
+
+.login-container {
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 
 .login-card {
@@ -153,7 +277,7 @@ async function handleLogin() {
 .login-button {
   width: 100%;
   padding: 0.75rem;
-  background-color: var(--second-color);
+  background-color: var(--primary-color);
   color: white;
   border: none;
   border-radius: 4px;
@@ -163,11 +287,22 @@ async function handleLogin() {
 }
 
 .login-button:hover {
-  background-color: var(--hover-second-color);
+  background-color: var(--neutral-color-04);
 }
 
 .login-button:disabled {
   background-color: #a5a5a5;
   cursor: not-allowed;
+}
+
+.toggle-mode {
+  text-align: center;
+  margin-top: 1rem;
+}
+
+.toggle-mode a {
+  color: var(--secondary-color-blue);
+  font-weight: bold;
+  cursor: pointer;
 }
 </style>
