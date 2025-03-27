b6481b640c4c8db344c8d46ed82753655ed3fdc5
Guilherme Rogerio Lavanholi Bertagna <guirogeriobertagna00@gmail.com>
2025-03-26 21:34:20 -0300

    alterações do card

--- src/components/CardComponentProducts.vue
+++ src/components/CardComponentProducts.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>
+  <div class="all-cards">
     <div class="products-card" v-for="products in allProducts" :key="products.id">
-      <div>
+      <div class="img-product">
            {{products.name}} 
-           {{ products.id }}  
+      </div>
+      <div class="button-product">
+        <button>Add to cart</button>
+      </div>
+      <div class="rating">
+        esrtrelinhas
+      </div>
+      <div class="name-discription">
+          <h4>  {{products.name}} </h4>
+          <h5>{{ products.discribe }}</h5>
+          <h5>{{ products.price }}</h5>
       </div>
     </div>
   </div>
@@ -25,7 +35,16 @@ onMounted(async () => {
 </script>
 
 <style scoped>
-
+.all-cards{
+  display: flex;
+  gap: 30px;
+  align-items: center;
+  justify-content: center;
+}
+.products-card{
+  border: 3px solid black;
+  
+}
 
 </style>
 
