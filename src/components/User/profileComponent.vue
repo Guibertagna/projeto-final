c5d7e29336798482ea91debda8ea8958bfe8c2f8
Guilherme Rogerio Lavanholi Bertagna <guirogeriobertagna00@gmail.com>
2025-04-22 18:52:05 -0300

    all

--- src/components/User/profileComponent.vue
+++ src/components/User/profileComponent.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="allContent">
-
+  <div class="all-content">
+    <div v-if="userStore.user.role == 'ADMIN'">
+        <button class="button-create">Create new product</button>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { useAuthenticateStore } from '@/stores/authenticate';
+import { onMounted } from 'vue';
+
+const userStore = useAuthenticateStore()
+onMounted(()=>{
+    console.log(userStore.user.role)
+})
+
+
+
+
 </script>
 
 <style scoped>
 
 </style>
+
